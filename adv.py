24454a33f2bad0474bc24a99b8d5d1890969ae05
unknown <ccurry20@gmail.com>
2020-08-02 22:13:05 -0400

    Completed MVP

--- adv.py
+++ adv.py
@@ -29,6 +29,44 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# move the player:
+def move_player(direction):
+    player.travel(direction) # check players direction options
+    # keep adding to traversal path
+    traversal_path.append(direction) # n, n, 
+
+# DFT traversal
+def DFT_recursive(visited=None, prev=None, move=None):
+    current_room = player.current_room.id # grab the current room
+    # grab all possible EXITS for current room
+    neighbors = player.current_room.get_exits() 
+    reverse = {'w': 'e', 'n': 's', 's': 'n', 'e': 'w'}
+
+    if visited == None:
+         visited = {}
+
+    # current room is not in visited, add to visited
+    if current_room not in visited: 
+        visited[current_room] = {}
+
+    # if there is direction (move) add to visited as current node (vertex)
+    if move is not None: 
+        visited[prev][move] = current_room
+
+    if prev is not None: # if prev was 'N' reverse must be 'S'
+        visited[current_room][reverse[move]] = prev # add reverse to visited direction
+
+    if len(visited[current_room]) < len(neighbors): # if visited current has more exits
+        for direction in neighbors: # 'n', 's', 'e', 'w' possible exits
+            if direction not in visited[current_room]: # if direction to move not in current room
+                move_player(direction) # move player to that direction and add to traversal path
+                DFT_recursive(visited, prev=current_room, move=direction) #recurse
+
+    if len(visited) < len(room_graph): # if more roooms
+        direction = reverse[move]
+        move_player(direction)
+
+DFT_recursive() # invoke function with default
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
